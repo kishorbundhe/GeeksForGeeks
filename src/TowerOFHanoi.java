6ef14f377ac540506b1068bc4bdc8ee217dd5a00
=Kishor Bundhe <=kishor.bundhe@techniatranscat.com>
2019-09-16 23:48:15 +0530

    Tower of Hanoi

---
+++ src/TowerOFHanoi.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class TowerOFHanoi {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int N = sc.nextInt();
+            Hanoi(N, 1, 3, 2);
+        }
+    }
+
+    static int count = 0, moves = 0;
+
+    public static void Hanoi(int N, int from, int to, int aux) {
+
+        if (N == 1) {
+            moves++;
+
+            System.out.println("move disk " + N + " from rod " + from + " to rod " + to);
+
+            return;
+        }
+
+        Hanoi(N - 1, from, aux, to);
+        moves++;
+        System.out.println("move disk " + N + " from rod " + from + " to rod " + to);
+        Hanoi(N - 1, aux, to, from);
+    }
+}
