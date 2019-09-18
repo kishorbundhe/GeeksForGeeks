bcc2655df8ad6488ee322500c71f397483a74332
=Kishor Bundhe <=kishor.bundhe@techniatranscat.com>
2019-09-18 23:33:06 +0530

    JosephProblem Recursive Solution

---
+++ src/JosephProblem.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class JosephProblem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int n, k;
+        while (t-- > 0) {
+            n = sc.nextInt();
+            k = sc.nextInt();
+
+            System.out.println(josephus(n, k));
+        }
+    }
+
+    private static int josephus(int n, int k) {
+        if (n == 1) return 1;
+        return ((josephus(n - 1, k) + k - 1) % n) + 1;
+    }
+}
