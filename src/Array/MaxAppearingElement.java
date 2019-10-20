db96883a6a0739236b4630984ac58768819070a9
=Kishor Bundhe <=kishor.bundhe@techniatranscat.com>
2019-10-20 12:08:27 +0530

    This Program determines the most occuring element in the ranges given .

---
+++ src/Array/MaxAppearingElement.java
@@ -0,0 +1,77 @@
+package Array;
+
+import java.util.*;
+
+/* Given n ranges find the max element appearing in these ranges  Example l[]={1,2,5,15} and
+r[]={5,8,7,18} and 0<<l[i]&&r[i] <<1000 then output will be 5 as 5 appears in the range 1 to 5 and 2to8 and 5to7
+i.e is 3 times */
+class Coordinate {
+    int startpoint;
+    int endpoint;
+
+    Coordinate(int startpoint, int endpoint) {
+        this.startpoint = startpoint;
+        this.endpoint = endpoint;
+    }
+}
+
+
+public class MaxAppearingElement {
+    public static void main(String[] args) {
+        System.out.println("No of ranges you want to enter");
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int i = 0, left, right;
+        Set<Coordinate> setsOfRanges = new HashSet<>();
+        System.out.println("Enter " + size + " no of Ranges");
+        while (i < size) {
+            left = sc.nextInt();
+            right = sc.nextInt();
+            Coordinate temp = new Coordinate(left, right);
+            setsOfRanges.add(temp);
+            i++;
+        }
+        maxappearedElement(setsOfRanges);
+    }
+
+    /*Creating Prefix Array*/
+    public static ArrayList createPrefixList(ArrayList<Integer> temp) {
+
+        ArrayList<Integer> prefixList = new ArrayList<Integer>(50);
+        prefixList.add(temp.get(0));
+        for (int i = 1; i < temp.size(); i++) {
+            prefixList.add(i, prefixList.get(i - 1) + temp.get(i));
+        }
+        return prefixList;
+    }
+
+    public static void maxappearedElement(Set<Coordinate> temp) {
+        int[] arr = new int[50];
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i : arr) {
+            list.add(i);
+        }
+
+        for (Coordinate i : temp) {
+            list.set(i.startpoint, list.get(i.startpoint) + 1);
+            list.set(i.endpoint + 1, list.get(i.endpoint + 1) - 1);
+        }
+        System.out.println("list Before" + list);
+        list = createPrefixList(list);
+        System.out.println("Prefix list" + list);
+        int count = 0;
+        List<Integer> maxlist = new ArrayList<Integer>();
+        for (int i = 0; i < list.size(); i++)
+            if (list.get(i) == count) {
+                maxlist.add(i);
+                count = list.get(i);
+            } else if (list.get(i) > count) {
+                maxlist.clear();
+                count = list.get(i);
+                maxlist.add(i);
+            }
+        System.out.println("The list of element Occuring most i.e =" + count + " of times =" + maxlist);
+    }
+
+}
+
