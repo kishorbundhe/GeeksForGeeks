a308871d6979af4535a1aa36b78cb5fea166f48f
=Kishor Bundhe <=kishor.bundhe@techniatranscat.com>
2019-11-04 17:33:02 +0530

    Public Function which can be used in many programs

---
+++ src/PublicFunctions/PublicFunctions.java
@@ -0,0 +1,18 @@
+package PublicFunctions;
+
+public class PublicFunctions {
+
+    public static int gcd(int a, int b) {
+
+        while (a != b) {
+            if (a > b) {
+                a = a - b;
+            } else {
+                b = b - a;
+            }
+        }
+        return a;
+    }
+
+
+}
