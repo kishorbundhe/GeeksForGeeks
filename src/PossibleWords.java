25a6edb87095b65733ceaeb8f21a9a929463a268
=Kishor Bundhe <=kishor.bundhe@techniatranscat.com>
2019-11-20 07:58:16 +0530

    O(n) function to cal leader in a array

--- src/PossibleWords.java
+++ src/PossibleWords.java
@@ -6,14 +6,14 @@ import java.io.*;
 import java.lang.*;
 
 public class PossibleWords {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
 
 
         while (t-- > 0) {
             int n = sc.nextInt(); // input size of array
-            int arr[] = new int[n]; //input the elements of array that are keys to be pressed
+            int[] arr = new int[n]; //input the elements of array that are keys to be pressed
 
             for (int i = 0; i < n; i++)
                 arr[i] = sc.nextInt();
@@ -24,22 +24,18 @@ public class PossibleWords {
     }
 }
 
-
-/*This is a function problem.You only need to complete the function given below*/
-//User function Template for Java
 class PhoneDigit {
-    static String[] key = {null, null, "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    String[] key = {null, null, "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
-    static void possibleWords(int a[], int N) {
+    void possibleWords(int[] a, int N) {
+
         String str = key[a[0]];
         for (int i = 0; i < key[a[0]].length(); i++) {
-
             printPatterns(" " + str.charAt(i), 0, a);
         }
-
     }
 
-    static void printPatterns(String Current, int index, int a[]) {
+    void printPatterns(String Current, int index, int[] a) {
         String temp = "";
         if (index == a.length - 1) {
             System.out.print(Current + "");
@@ -50,5 +46,4 @@ class PhoneDigit {
             printPatterns(Current + temp.charAt(i), index + 1, a);
         }
     }
-
 }
