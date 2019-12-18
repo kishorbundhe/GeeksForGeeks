15a52aca80aa022b433a20182be2450a019ae81f
=Kishor Bundhe <=kishor.bundhe@techniatranscat.com>
2019-12-18 18:50:21 +0530

    Program : Find the Peak Element in the array

---
+++ src/Search/Peakproblem.java
@@ -0,0 +1,54 @@
+package Search;
+/*Given an array A of N integers. The task is to find a peak element in it.
+An array element is peak if it is not smaller than its neighbours. For corner elements, we need to consider only one neighbour.*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Peakproblem {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            String[] string = br.readLine().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(string[i]);
+            }
+            findpeak(arr, n);
+        }
+    }
+
+    private static void findpeak(int[] arr, int size) {
+        int mid, high, low;
+        low = 0;
+        high = size - 1;
+        if (size == 1) {
+            System.out.println(0);
+            return;
+        } else if (arr[0] >= arr[1]) {
+            System.out.println(0);
+            return;
+        } else if (arr[high] >= arr[high - 1]) {
+            System.out.println(high);
+            return;
+        }
+
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            if (mid == 0 || mid == size - 1) {
+                break;
+            } else if (arr[mid] >= arr[mid - 1] && arr[mid] >= arr[mid + 1]) {
+                System.out.println(mid);
+                return;
+            } else if (arr[mid] < arr[mid + 1]) {
+                low = mid + 1;
+            } else if (arr[mid] > arr[mid - 1]) {
+                high = mid - 1;
+            }
+        }
+
+    }
+}
