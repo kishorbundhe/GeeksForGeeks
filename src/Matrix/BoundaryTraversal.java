712f48a2e9e1481d37160563afea9f568e730df5
Kishor Bundhe <kishorbundhe@gmail.com>
2020-02-25 08:22:18 +0530

    Boundary Traversal

--- src/Matrix/BoundaryTraversal.java
+++ src/Matrix/BoundaryTraversal.java
@@ -3,7 +3,6 @@ package Matrix;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 /* Given a matrix we need to traverse the boundaries of it*/
 public class BoundaryTraversal {
@@ -25,34 +24,66 @@ public class BoundaryTraversal {
         }
     }
 
-    private static void boundaryTraversal(int[][] matrixOne) {
+    /*   private static void boundaryTraversal(int[][] matrixOne) {
 
-        int i = 0, j = 0, noOfRows, nofColumns;
-        noOfRows = matrixOne.length - 1;
-        nofColumns = matrixOne[0].length - 1;
-        int flag = 0;
-        while (true) {
-            if (i == 0 && j == 0) {
-                if (flag == 3) {
-                    break;
-                }
-                System.out.println(Arrays.toString(matrixOne[0]));
-                j = nofColumns;
-                i++;
-            } else if (j == (nofColumns) && i < (noOfRows)) {
-                System.out.print(matrixOne[i][j] + " ");
-                i++;
-            } else if (i == (noOfRows) && j > 0) {
-                System.out.print(matrixOne[i][j] + " ");
-                j--;
-            } else if (j == 0 && i > 0) {
-                System.out.print(matrixOne[i][j] + " ");
-                i--;
-                flag = 3;
+           int i = 0, j = 0, noOfRows, nofColumns;
+           noOfRows = matrixOne.length - 1;
+           nofColumns = matrixOne[0].length - 1;
+           int flag = 0;
+           while (true) {
+               if (i == 0 && j != nofColumns) {
+                   if (flag == 3) {
+                       break;
+                   }
+                   System.out.print(matrixOne[i][j] + " ");
+                   //System.out.println(Arrays.toString(matrixOne[0]));
+                   j++;
+               } else if (j == (nofColumns) && i < (noOfRows)) {
+
+                   System.out.print(matrixOne[i][j] + " ");
+                   i++;
+
+               } else if (i == (noOfRows) && j > 0) {
+
+                   System.out.print(matrixOne[i][j] + " ");
+                   j--;
+               } else if (j == 0 && i > 0) {
+
+                   System.out.print(matrixOne[i][j] + " ");
+                   i--;
+                   flag = 3;
+               }
+           }
+
+       }*/
+    // Submitted Solution
+    private static void boundaryTraversal(int[][] matrix) {
+        int top, right, left, bottom, i, j;
+        int r = matrix.length - 1, c = matrix[0].length - 1;
+        top = 0;
+        left = 0;
+        right = c;
+        bottom = r;
+        for (j = 0; j <= right; j++) {
+            System.out.print(matrix[0][j] + " ");
+        }
+        top++;
+        for (i = top; i <= bottom; i++) {
+            System.out.print(matrix[i][right] + " ");
+        }
+        right--;
+        if (top <= bottom) {
+            for (j = right; j >= left; j--) {
+                System.out.print(matrix[bottom][j] + " ");
+            }
+        }
+        bottom--;
+        if (right >= left) {
+            for (i = bottom; i >= top; i--) {
+                System.out.print(matrix[i][left] + " ");
             }
         }
 
     }
-
 
 }
